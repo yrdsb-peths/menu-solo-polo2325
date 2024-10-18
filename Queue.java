813d76ac56c748b2612d95d989d6789ab3611170
polo2325 <flipflop2325@outlook.com>
2024-10-17 22:54:25 -0400

    Fix some mistakes

---
+++ Queue.java
@@ -0,0 +1,45 @@
+public class Queue<Type>
+{
+    private Node first, last;
+    
+    private class Node
+    {
+        Type item; 
+        Node next;
+    }
+
+    public boolean isEmpty()
+    {
+        return first == null;
+    }
+
+    public void enqueue(Type item)
+    {
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        
+        if(isEmpty())
+        {
+            first = last;
+        }
+        else
+        {
+            oldLast.next = last;
+        }
+    }
+
+    public Type dequeue()
+    {
+        Type item = first.item;
+        first = first.next;
+
+        if(isEmpty())
+        {
+            first = last;
+        }
+
+        return item;
+    }
+}
