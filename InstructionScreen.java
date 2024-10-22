c87db643ded108ce2813ee9a9e9322bfde7ac360
polo2325 <flipflop2325@outlook.com>
2024-10-21 21:24:44 -0400

    cycle through instruction screens

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -2,6 +2,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class InstructionScreen extends World
 {
+    private String[] instructionsList = {"Instruction 1", "Instruction 2", "Instruction 3"};
+    private Label instructions;
+    private int screenIndex = 0;
 
     /**
      * Constructor for objects of class InstructionScreen.
@@ -12,12 +15,43 @@ public class InstructionScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
 
-        writeInsturctions();
+
+        instructions = new Label(instructionsList[screenIndex], 50);
+        addObject(instructions, 300, 200);
+        
+        Button navigateLeftButton = new Button(this::navigateLeft);
+        Button navigateRightButton = new Button(this::navigateRight);
+
+        addObject(navigateLeftButton, 100, 340);
+        addObject(navigateRightButton, 500, 340);
     }
 
-    public void writeInsturctions()
+    public void writeInstructions()
     {
-        Label instructions = new Label("Instructions goes here", 50);
-        addObject(instructions, 300, 200);
+        instructions.setValue(instructionsList[screenIndex]);
+    }
+
+    public void navigateLeft()
+    {
+        screenIndex--;
+        
+        if(screenIndex < 0)
+        {
+            screenIndex = 2;
+        }
+        
+        writeInstructions();
+    }
+
+    public void navigateRight()
+    {
+        screenIndex++;
+        
+        if(screenIndex > 2)
+        {
+            screenIndex = 0;
+        }
+        
+        writeInstructions();
     }
 }
